7c283630a138e4a8c42ca11575c42133ef05d016
wallbroken <diomede979@gmail.com>
2021-04-20 03:55:49 +0200

    model setup

--- shoes/models.py
+++ shoes/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Shoe(models.Model):
     user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     model_name = models.CharField(max_length=200)
-    brand = models.CharField()
-    size = models.IntegerField(default=0)
+    brand = models.CharField(max_length=200)
+    size = models.FloatField(default=0)
     heel_height = models.IntegerField(default=0)
     image = models.ImageField()
