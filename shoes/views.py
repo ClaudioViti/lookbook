9107a219d1f62b73a82355591c2096fbae0a8b8f
wallbroken <diomede979@gmail.com>
2021-04-20 04:07:42 +0200

    model class

--- shoes/views.py
+++ shoes/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from . import models
+
+from django.views.generic import ListView
+
+class ShoeListView(ListView):
+
+    model = models.Shoe
