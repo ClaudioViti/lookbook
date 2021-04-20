1f1b8458ff7c7361d5e3ce93e1287c8000a03ac4
wallbroken <diomede979@gmail.com>
2021-04-20 03:30:52 +0200

    model definition

--- shoes/models.py
+++ shoes/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Shoe(models.Model):
+    model_id = models.ForeignKey(ID, on_delete=models.CASCADE)
+    model_name = models.CharField(max_length=200)
+    brand = models.IntegerField(default=0)
+    size = models.IntegerField(default=0)
+    heelHeight = models.IntegerField(default=0)
+    imageURL = models.URL()
