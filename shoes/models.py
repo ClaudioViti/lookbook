75c4de2e37c36a94ef007cc8a03c58402d5d89d0
wallbroken <diomede979@gmail.com>
2021-04-21 14:07:06 +0200

    added fields

--- shoes/models.py
+++ shoes/models.py
@@ -4,8 +4,16 @@ from django.db import models
 
 class Shoe(models.Model):
     user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
-    model_name = models.CharField(max_length=200)
+    style = models.CharField(max_length=200)
     brand = models.CharField(max_length=200)
     size = models.FloatField(default=0)
     heel_height = models.IntegerField(default=0)
+    heel_kind = models.CharField(max_length=20)
+    plateau_heitht = models.IntegerField(default=0)
+    season = models.CharField(max_length=10)
+    year = models.FloatField(default=0)
+    comfortable = models.IntegerField(default=0)
+    def real_heel(self):
+        return self.heel_height - self.plateau_heitht
+
     image = models.ImageField()
