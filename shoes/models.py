0a43c653c05b9605f79085bbbe5865a8d03f9e90
wallbroken <diomede979@gmail.com>
2021-04-21 16:43:12 +0200

    added new fields

--- shoes/models.py
+++ shoes/models.py
@@ -4,16 +4,21 @@ from django.db import models
 
 class Shoe(models.Model):
     user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
-    style = models.CharField(max_length=200)
-    brand = models.CharField(max_length=200)
+    model = models.CharField(max_length=200, blank=True, null=True)
+    style = models.CharField(max_length=200, blank=True, null=True)
+    brand = models.CharField(max_length=200, blank=True, null=True)
     size = models.FloatField(default=0)
-    heel_height = models.IntegerField(default=0)
-    heel_kind = models.CharField(max_length=20)
-    plateau_heitht = models.IntegerField(default=0)
-    season = models.CharField(max_length=10)
-    year = models.FloatField(default=0)
-    comfortable = models.IntegerField(default=0)
+    color = models.CharField(max_length=200, blank=True, null=True)
+    heel_height = models.FloatField(default=0, blank=True, null=True)
+    heel_kind = models.CharField(max_length=20, blank=True, null=True)
+    plateau_height = models.FloatField(default=0, blank=True, null=True)
+    material = models.CharField(max_length=200, blank=True, null=True)
+    season = models.CharField(max_length=10, blank=True, null=True)
+    year = models.IntegerField(default=0)
+    comfort = models.IntegerField(default=None, blank=True, null=True)
+    state = models.CharField(max_length=200, default="New", blank=True, null=True)
+    available = models.BooleanField(default=True)
     def real_heel(self):
-        return self.heel_height - self.plateau_heitht
+        return self.heel_height - self.plateau_height
 
     image = models.ImageField()
