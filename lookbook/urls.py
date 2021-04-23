35665cb44f245bfb7102b3ff9b1b7f66aad8f21c
wallbroken <diomede979@gmail.com>
2021-04-23 23:52:11 +0200

    fix

--- lookbook/urls.py
+++ lookbook/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-from shoes.views import ShoeListView, ColorView
+from shoes.views import ShoeListView
 from django.conf import settings
 from django.conf.urls.static import static
 
--- shoes/views.py
+++ shoes/views.py
@@ -6,20 +6,14 @@ from . import models
 
 from django.views.generic import ListView
 
-from django.shortcuts import get_object_or_404
-from django.views.generic import ListView
-from shoes.models import Shoe
-
 class ShoeListView(ListView):
 
     model = models.Shoe
     template_name = 'shoes/shoes_list.html'
 
-    def get_queryset(self):
-        qs = super().get_queryset()
-
-        color = self.request.GET.get('color')
-        if color:
-            qs = qs.filter(color=color)
-​
-        return qs
+def get_queryset(self):
+    qs = super().get_queryset()
+    color = self.request.GET.get('color')
+    if color:
+        qs = qs.filter(color=color)
+    return qs
