7ddaeeba30a924c703ae078dde99d04c862db05f
wallbroken <diomede979@gmail.com>
2021-04-24 00:32:08 +0200

    fix

--- shoes/views.py
+++ shoes/views.py
@@ -11,10 +11,15 @@ class ShoeListView(ListView):
     model = models.Shoe
     template_name = 'shoes/shoes_list.html'
 
+FILTER_FIELDS = ['color', 'size', 'model']
+​
+​
     def get_queryset(self):
         qs = super().get_queryset()
-        x = self.request.GET.get(field)
-        if x:
-            qs = qs.filter(color=x)
-            qs = qs.filter(model=x)
+​
+        for field in FILTER_FIELDS:
+            value = self.request.GET.get(field)
+            if value:
+                qs = qs.filter(**{field: value})
+​
         return qs
