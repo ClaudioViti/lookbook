c2d303b5512407a7271054be366730452ceb112e
wallbroken <diomede979@gmail.com>
2021-05-02 10:07:59 +0200

    wip

--- shoes/views.py
+++ shoes/views.py
@@ -65,7 +65,9 @@ class ShoeUpdateView(LoginRequiredMixin, UpdateView):
     fields = '__all__'
     template_name = 'shoes/manage/shoe_form.html'
     model = models.Shoe
-
+    def get_success_url(self):
+        
+        return reverse_lazy( {% url 'ShoeManageView' %} )
 
 class ShoeDeleteView(LoginRequiredMixin, DeleteView):
 
