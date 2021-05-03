9843c3cfb3cd3a57a33359e9f8ef23e32835505b
wallbroken <diomede979@gmail.com>
2021-05-04 01:19:55 +0200

    wip

--- shoes/forms.py
+++ shoes/forms.py
@@ -3,7 +3,7 @@ from shoes.models import Shoe
 from django import forms
 
 # Create the form class.
-class SearchImage(ModelForm):
+class SearchForm(ModelForm):
      class Meta:
          model = Shoe
          fields = ['color', 'size', 'model', 'style', 'toe', 'slingback', 'platform', 'favourite']
