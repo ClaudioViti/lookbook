908ef0aa7b8cf6df91d3163e1a70a32ffe67335f
wallbroken <diomede979@gmail.com>
2021-05-03 11:32:27 +0200

    wip

--- shoes/forms.py
+++ shoes/forms.py
@@ -1,18 +1,8 @@
 from django.forms import ModelForm
 from shoes.models import Shoe
-from django.forms import BaseModelFormSet
 
 # Create the form class.
 class ShoesForm(ModelForm):
      class Meta:
          model = Shoe
          fields = ['color', 'size', 'model', 'style', 'toe', 'slingback', 'platform', 'favourite']
-
-
-
-
-
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        
