36e89f740318bd510e0e94b8a8732f62b8058352
wallbroken <diomede979@gmail.com>
2021-05-03 10:27:50 +0200

    wip

--- shoes/forms.py
+++ shoes/forms.py
@@ -6,3 +6,12 @@ class ShoesForm(ModelForm):
      class Meta:
          model = Shoe
          fields = ['color', 'size', 'model', 'style', 'toe', 'slingback', 'platform', 'favourite']
+
+
+from django.forms import BaseModelFormSet
+
+
+class ImageFormSet(ImageFormSet):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.queryset = Shoe.objects.filter(name__startswith='O')
