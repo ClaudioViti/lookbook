2e8c5f8685061f24bcfa22dda3ee4d623236a678
wallbroken <diomede979@gmail.com>
2021-05-21 22:44:18 +0200

    .

--- shoes/forms.py
+++ shoes/forms.py
@@ -17,6 +17,7 @@ class ShoeForm(ModelForm):
 
 ShoeImageFormSet = modelformset_factory(ShoeImage, fields=('image',), extra=3)
 ShoeImageInlineFormset = inlineformset_factory(Shoe, ShoeImage, fields=('image',))
+cart = forms.BooleanField(required=False)
 
 class ShoeOrderForm(forms.Form):
     order = forms.ChoiceField(choices=(('', '---------'), ('pk', 'ID'), ('-year', 'Newer'),('year', 'Older'), ('-heel_height', 'High Heel'), ('heel_height', 'Low Heel'), ('comfort', 'Comfort')), required=False)
