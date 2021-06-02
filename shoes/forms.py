4539771172812a0b66aa8d166a440520e30d315c
wallbroken <diomede979@gmail.com>
2021-06-03 00:19:16 +0200

    fields dependency in admin minicart

--- shoes/forms.py
+++ shoes/forms.py
@@ -3,6 +3,8 @@ from shoes.models import Shoe, ShoeImage, ShoeBrand
 from django import forms
 from django.forms.models import modelformset_factory
 from django.forms import inlineformset_factory
+from django.core.exceptions import ValidationError
+
 
 # Create the form class.
 class SearchForm(ModelForm):
@@ -52,7 +54,31 @@ class ShoeCartsForm(ModelForm):
         model = Shoe
         fields = ['cart_user', 'urgent_user', 'ordered_user', 'delivered_user', 'id']
         widgets = {'id': forms.HiddenInput()}
+    def clean(self):
+        cleaned_data = super().clean()
+        delivered_users = cleaned_data.get("delivered_user")  # fill the field name
+        ordered_users = cleaned_data.get("ordered_user")  # fill the field name
+        cart_users = cleaned_data.get("cart_user")  # fill the field name
+        urgent_users = cleaned_data.get("urgent_user")  # fill the field name
+        
+        errors = []
+        for user in delivered_users:
+            if user not in ordered_users:
+                errors.append(
+                    ValidationError('User %(name)s is not in list of users who made order',
+                                    params={'name': user.username}),
+                ) 
+        if errors:
+            raise ValidationError(errors)
 
+        for user in urgent_users:
+            if user not in cart_users:
+                errors.append(
+                    ValidationError('User %(name)s is not in list of users in the cart',
+                                    params={'name': user.username}),
+                ) 
+        if errors:
+            raise ValidationError(errors)
 
 class ShoeFavouriteForm(ModelForm):
     class Meta:
